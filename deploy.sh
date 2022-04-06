1e95824d00d2daed45dd0e6e4d46a4226f57173a
lin lin <linlin152@foxmail.com>
2022-04-06 21:38:44 +0800

    upgrade next and hexo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 hexo generate
-cp -R public/* .deploy/linsite.github.io
-cd .deploy/linsite.github.io
+cp -R public/* deploy/linsite.github.io
+cp source/CNAME public
+cd deploy/linsite.github.io
 git add .
 git commit -m “update”
-git push origin master
+#git push origin master
